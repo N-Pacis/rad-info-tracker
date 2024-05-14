32094fd4e7f442600023f4b9a9594ba8f8189478
nkubito-pacis <74002896+N-Pacis@users.noreply.github.com>
2024-05-14 11:03:03 +0200

    fix cors

--- src/main/java/rw/auca/radinfotracker/config/WebConfig.java
+++ src/main/java/rw/auca/radinfotracker/config/WebConfig.java
@@ -9,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").addResourceLocations("classpath:/static");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("PUT", "DELETE", "POST", "GET")
+                .allowedHeaders("Authorization", "header3", "Origin", "Access-Control-Allow-Origin", "Content-Type",
+                        "Accept", "Origin, Accept", "X-Requested-With",
+                        "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .exposedHeaders("Origin", "Content-Type", "Accept", "Authorization",
+                        "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true);
     }
 }
