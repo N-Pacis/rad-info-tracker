dc7282f975366667591e03c13662d43150de0426
nkubito-pacis <74002896+N-Pacis@users.noreply.github.com>
2024-05-14 09:23:11 +0200

    [Fix] Fixed cors configuration

--- src/main/java/rw/auca/radinfotracker/RadInfoTrackerApplication.java
+++ src/main/java/rw/auca/radinfotracker/RadInfoTrackerApplication.java
@@ -2,6 +2,9 @@ package rw.auca.radinfotracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RadInfoTrackerApplication {
@@ -10,4 +13,14 @@ public class RadInfoTrackerApplication {
         SpringApplication.run(RadInfoTrackerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**").allowedOrigins("**").allowedMethods("*");
+            }
+        };
+    }
+    
 }
--- src/main/java/rw/auca/radinfotracker/config/WebConfig.java
+++ src/main/java/rw/auca/radinfotracker/config/WebConfig.java
@@ -1,17 +1,19 @@
 package rw.auca.radinfotracker.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedHeaders("*")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST","PUT", "DELETE","OPTIONS");
+        registry.addMapping("/**").allowedMethods("*");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").addResourceLocations("classpath:/static");
+    }
 }
