6fb5f8ae1d1bbe1ff1d12a39dc56528c8085714d
nkubito-pacis <74002896+N-Pacis@users.noreply.github.com>
2024-05-30 14:24:24 +0200

    Update SecurityConfig.java

--- src/main/java/rw/auca/radinfotracker/config/SecurityConfig.java
+++ src/main/java/rw/auca/radinfotracker/config/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig {
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	public static final String[] AUTH_WHITELIST = {
 			"/api/v1/auth/signin",
+			"/api/v1/files/raw/**",
+			"/api/v1/files/download/**",
 			"/swagger-resources",
 			"/swagger-resources/**",
 			"/configuration/ui",
